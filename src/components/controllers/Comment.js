f52b1814ecac58e6e356c8c5ce5f9ee836f5eaa9
Szymon <szymonrozmarynowski@gmail.com>
2019-11-15 04:53:12 +0100

    comment structure change

--- src/components/controllers/Comment.js
+++ src/components/controllers/Comment.js
@@ -2,32 +2,13 @@ import React from 'react';
 import CommentView from '../views/CommentView'
 
 class Comment extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            author: "",
-            content: "",
-            time: ""
-        }
-    }
-
-    componentDidMount() {
-        //TODO: sciagniecie z bazy informacji o commencie o danym ID
-        const res = "" //zaciagniete info
-        this.setState ({
-            author: res.author,
-            content: res.content,
-            time: res.time
-        })
-
-    }
 
     render() {
         return (
             <CommentView
-                author = {this.state.author}
-                content = {this.state.content}
-                time = {this.state.time}
+                author = {this.props.author}
+                content = {this.props.content}
+                time = {this.props.time}
             />
         )
     }
