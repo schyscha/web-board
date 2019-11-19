60dfd776620211bdf8567fff0fc1a8bca8f677f1
Eryk <erykkrupa1@gmail.com>
2019-11-19 03:10:44 +0100

    Add nickname support

--- src/components/structure/Navbar.js
+++ src/components/structure/Navbar.js
@@ -1,16 +1,25 @@
 import React from "react"
 import {Navbar} from "react-bootstrap";
+import {getNick} from "./App"
+
 require("../../styles/Navbar.css")
 
 function NavBar() {
     return (
         <div>
             <Navbar variant="dark" sticky="top" expand="md">
+                <div className="hello">
+                    <span>Witaj&nbsp;</span>
+                    <span id="nick"></span>
+                    <span>!</span>
+                    <form>
+                        <input type="button" value="Zmień nick" onClick={getNick}/>
+                    </form>
+                </div>
                 <div className="header">Web Board</div>
             </Navbar>
         </div>
     )
-
 }
 
 export default NavBar
