d7b86513c0696411f62be0f05b2757e0080c8269
Szymon <szymonrozmarynowski@gmail.com>
2019-11-19 03:32:44 +0100

    add styles for other components with appropriate margin-shift, clean project

--- src/components/views/NotFoundView.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-require("../../styles/NotFound.css")
-
-export default () => (
-    <div className="NotFound">
-        <h1>Sorry, page not found! :(</h1>
-    </div>
-);
